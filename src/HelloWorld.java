5db035775bf79693254ef15ef860f4519350bda7
Sagar <sagarenfy350@gmail.com>
2024-04-05 17:01:33 +0530

    New code with deletion

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,18 +3,7 @@ public class HelloWorld {
         int j=10;
         for(int i=0;i<=j;i++){
           System.out.println(i);
-
-
-            System.out.println("Named");
-            System.out.println("Unnamed");
-            System.out.println("Named");
-            System.out.println("Unnamed");
-            System.out.println("This code is from gitX");
-            System.out.println("This code is from gitX");
-            System.out.println("This code is from gitX");
-            System.out.println("This code is from gitX");
-            System.out.println("This code is from gitX");
-            System.out.println("This code is from gitX");
+          System.out.println("Git A");
 
       }
     }
